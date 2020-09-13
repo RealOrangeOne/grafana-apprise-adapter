2cd71cf12beeb72de16efb8d2e346016497526e2
Jake Howard <git@theorangeone.net>
2020-09-13 12:30:23 +0100

    Use unwrap_or_else

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,7 @@ use std::env;
 
 pub fn get_port() -> u16 {
     return env::var("PORT")
-        .unwrap_or("5000".into())
+        .unwrap_or_else(|_| "5000".into())
         .parse::<u16>()
         .expect("Invalid port number");
 }
