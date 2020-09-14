7645561547a4b2aae11cab03aa8df26567cf52d1
Jake Howard <git@theorangeone.net>
2020-09-14 17:38:41 +0100

    Pass through authorization header

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use actix_web::client::Client;
+use actix_web::http::header;
 use actix_web::middleware::Logger;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use env_logger::Env;
 use std::process::exit;
 use url::Url;
@@ -17,14 +18,23 @@ async fn notify(
     data: web::Json<grafana::GrafanaPayload>,
     key: web::Path<String>,
     state: web::Data<AppState>,
+    req: HttpRequest,
 ) -> impl Responder {
     let payload = apprise::ApprisePayload::from(data.into_inner());
+    let client = Client::default();
     let apprise_url = match apprise::get_apprise_notify_url(&state.apprise_url, &key) {
         Ok(url) => url,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let client = Client::default();
-    return match client.post(apprise_url.as_str()).send_json(&payload).await {
+    let authorization_header = req.headers().get(header::AUTHORIZATION);
+    return match client
+        .post(apprise_url.as_str())
+        .if_some(authorization_header, |header, builder| {
+            builder.set_header(header::AUTHORIZATION, header.clone())
+        })
+        .send_json(&payload)
+        .await
+    {
         Ok(response) => HttpResponse::new(response.status()),
         Err(_) => HttpResponse::BadGateway().finish(),
     };
