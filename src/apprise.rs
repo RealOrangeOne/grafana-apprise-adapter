46296e057379ed0de98b21d6b898be3f67f0940e
Jake Howard <git@theorangeone.net>
2021-01-23 21:27:07 +0000

    Give up on my hatred of implicit returns

--- src/apprise.rs
+++ src/apprise.rs
@@ -15,13 +15,13 @@ pub enum AppriseState {
 
 impl From<GrafanaState> for AppriseState {
     fn from(gf_state: GrafanaState) -> AppriseState {
-        return match gf_state {
+        match gf_state {
             GrafanaState::Ok => AppriseState::Success,
             GrafanaState::Paused => AppriseState::Info,
             GrafanaState::Alerting => AppriseState::Failure,
             GrafanaState::Pending => AppriseState::Info,
             GrafanaState::NoData => AppriseState::Warning,
-        };
+        }
     }
 }
 
@@ -36,21 +36,21 @@ pub struct ApprisePayload {
 
 impl From<GrafanaPayload> for ApprisePayload {
     fn from(gf_payload: GrafanaPayload) -> ApprisePayload {
-        return ApprisePayload {
+        ApprisePayload {
             title: gf_payload.title,
             body: gf_payload.message,
             notification_type: AppriseState::from(gf_payload.state),
-        };
+        }
     }
 }
 
 pub fn get_apprise_notify_url(host: &Url, key: &str) -> Result<Url, ParseError> {
-    return host.join(&format!("/notify/{}", key));
+    host.join(&format!("/notify/{}", key))
 }
 
 pub fn get_apprise_url() -> Option<Url> {
     let apprise_env = env::var("APPRISE_URL").ok()?;
-    return Url::parse(&apprise_env).ok();
+    Url::parse(&apprise_env).ok()
 }
 
 #[cfg(test)]
